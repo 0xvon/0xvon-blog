3685b56ff693b02e8c00760f5fca217ef7b3636a
masatsch <kingmasatojames@gmail.com>
2023-03-28 21:12:14 -0700

    style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import Head from "next/head";
 import { GetStaticProps } from "next";
 import { getSortedPostsData, PostData } from "../lib/posts";
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/index.module.css';
 
 export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
@@ -21,14 +23,19 @@ const Home: React.FC<HomeProps> = ({ allPostsData }) => {
       <Head>
         <title>0xvon</title>
       </Head>
-      <h1>0vxon</h1>
-      <ul>
-        {allPostsData.map(({ id, date, title }) => (
-          <li key={id}>
-            <a href={`/blog/${id}`}>{title}</a>
-          </li>
-        ))}
-      </ul>
+      <h1 className={styles.header}>0vxon</h1>
+      <div className={styles.postList}>
+          {allPostsData.map(({ id, date, title }) => (
+            <div key={id} className={styles.postListItem}>
+              <div className={styles.postItem}>
+                <p className={styles.postDate}>{date}</p>
+                <a href={`/blog/${id}`}>
+                  <p className={styles.postTitle}>{title}</p>
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
     </div>
   );
 };
